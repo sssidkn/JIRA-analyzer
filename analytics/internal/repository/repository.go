031969eab9d1bf9fde7bcc3fcd86a3efc38c0c1a
nutochk <kozakova.ann@mail.ru>
2025-12-03 21:36:41 +0300

    the final touches

--- analytics/internal/repository/repository.go
+++ analytics/internal/repository/repository.go
@@ -43,48 +43,60 @@ func (r *repo) MakeTaskOne(ctx context.Context, key string) (*[]dto.IssueTaskOne
 	}
 
 	var issues []dto.IssueTaskOne
-	query = `WITH time_categories AS (
-	  SELECT id,
-		CASE 
-		  WHEN timespent <= 3600 THEN '1 hour'
-		  WHEN timespent <= 18000 THEN '1-5 hours'
-		  WHEN timespent <= 36000 THEN '5-10 hours'
-		  WHEN timespent <= 72000 THEN '10-20 hours'
-		  WHEN timespent <= 86400 THEN '20-24 hours'
-		  WHEN timespent <= 172800 THEN '1-2 days'
-		  WHEN timespent <= 432000 THEN '2-5 days'
-		  WHEN timespent <= 864000 THEN '5-10 days'
-		  WHEN timespent <= 1296000 THEN '10-15 days'
- 		  WHEN timespent <= 1728000 THEN '15-20 days'
-          WHEN timespent <= 2592000 THEN '20-30 days' 
-		  ELSE 'more'
-		END AS time_category,
-		CASE 
-		  WHEN timespent <= 3600 THEN 1
-		  WHEN timespent <= 18000 THEN 2
-		  WHEN timespent <= 36000 THEN 3
-		  WHEN timespent <= 72000 THEN 4
-		  WHEN timespent <= 86400 THEN 5
-		  WHEN timespent <= 172800 THEN 6
-		  WHEN timespent <= 432000 THEN 7
-		  WHEN timespent <= 864000 THEN 8
-          WHEN timespent <= 1296000 THEN 9
-		  WHEN timespent <= 1728000 THEN 10
-          WHEN timespent <= 2592000 THEN 11
-		  ELSE 12
-		END AS category_order
-		  FROM issue 
-		  WHERE projectid = $1
-		  AND status IN ('Closed', 'Resolved')
-          AND timespent IS NOT NULL
-		)
-		SELECT 
-		  time_category,
-		  COUNT(id) AS task_count,
-		  category_order
-		FROM time_categories
-		GROUP BY time_category, category_order
-		ORDER BY category_order`
+	query = `
+WITH issues_with_time AS (
+    SELECT
+        id,
+        COALESCE(
+            timespent,
+            EXTRACT(EPOCH FROM (closedtime - createdtime))::bigint
+        ) AS eff_timespent
+    FROM issue
+    WHERE projectid = $1
+      AND status IN ('Closed', 'Resolved')
+),
+time_categories AS (
+    SELECT
+        id,
+        CASE 
+            WHEN eff_timespent <= 3600 THEN '1 hour'
+            WHEN eff_timespent <= 18000 THEN '1-5 hours'
+            WHEN eff_timespent <= 36000 THEN '5-10 hours'
+            WHEN eff_timespent <= 72000 THEN '10-20 hours'
+            WHEN eff_timespent <= 86400 THEN '20-24 hours'
+            WHEN eff_timespent <= 172800 THEN '1-2 days'
+            WHEN eff_timespent <= 432000 THEN '2-5 days'
+            WHEN eff_timespent <= 864000 THEN '5-10 days'
+            WHEN eff_timespent <= 1296000 THEN '10-15 days'
+            WHEN eff_timespent <= 1728000 THEN '15-20 days'
+            WHEN eff_timespent <= 2592000 THEN '20-30 days'
+            ELSE 'more than 30 days'
+        END AS time_category,
+        CASE 
+            WHEN eff_timespent <= 3600 THEN 1
+            WHEN eff_timespent <= 18000 THEN 2
+            WHEN eff_timespent <= 36000 THEN 3
+            WHEN eff_timespent <= 72000 THEN 4
+            WHEN eff_timespent <= 86400 THEN 5
+            WHEN eff_timespent <= 172800 THEN 6
+            WHEN eff_timespent <= 432000 THEN 7
+            WHEN eff_timespent <= 864000 THEN 8
+            WHEN eff_timespent <= 1296000 THEN 9
+            WHEN eff_timespent <= 1728000 THEN 10
+            WHEN eff_timespent <= 2592000 THEN 11
+            ELSE 12
+        END AS category_order
+    FROM issues_with_time
+    WHERE eff_timespent IS NOT NULL
+)
+SELECT
+    time_category,
+    COUNT(id) AS task_count,
+    category_order
+FROM time_categories
+GROUP BY time_category, category_order
+ORDER BY category_order;
+`
 
 	rows, err := tx.Query(ctx, query, id)
 	if err != nil {
